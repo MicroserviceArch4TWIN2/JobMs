e98a18da9558b85cc610d86425f90c8e0c500678
oumeima-ibnelfekih <oumeima.ibnelfekih@esprit.tn>
2026-02-14 09:03:24 +0100

    add eureka client config

--- src/main/java/esprit/jobms/JobMsApplication.java
+++ src/main/java/esprit/jobms/JobMsApplication.java
@@ -2,7 +2,9 @@ package esprit.jobms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class JobMsApplication {
 
